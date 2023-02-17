12191b21e13865d5deb40062dd29ab4b588679e6
mac <chukwuekeuzoma@gmail.com>
2023-02-17 15:26:11 +0100

    chore:imported phoneInput package

--- src/Components/UiInput.tsx
+++ src/Components/UiInput.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 import UiIcon from "./UiIcon";
 import UiField from "./UiField";
