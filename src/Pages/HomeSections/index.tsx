34288741a62fdc958cc2dc615dc41c7535e1503f
mac <chukwuekeuzoma@gmail.com>
2023-02-20 16:16:08 +0100

    chore:homepage

--- src/Pages/HomeSections/index.tsx
+++ src/Pages/HomeSections/index.tsx
@@ -3,12 +3,18 @@ import HomeSectionTwo from './HomeSectionTwo';
 import HomeSectionThree from './HomeSectionThree';
 import HomeSectionFour from './HomeSectionFour';
 import HomeSectionFive from './HomeSectionFive';
+import HomeSectionSix from './HomeSectionSix';
+import Faq from './Faq';
+import HomeSectionSeven from './HomeSectionSeven';
 
 export{
     HomeSectionOne,
     HomeSectionTwo,
     HomeSectionThree,
     HomeSectionFour,
-    HomeSectionFive
+    HomeSectionFive,
+    HomeSectionSix,
+    Faq,
+    HomeSectionSeven
 }
 
